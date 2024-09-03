69dd44333d70a0e302791253d5dd8b3f330402f6
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-09-03 20:25:46 +0545

    Update out_of_offcice.py

--- erp/Custom_Code/out_of_offcice.py
+++ erp/Custom_Code/out_of_offcice.py
@@ -1,1 +1,13 @@
+import frappe 
+from frappe.utils import today
 
+def on_update(doc, method):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_approver_id")
+    if purchase_approver == '' or purchase_approver == None and "Administrator" not in frappe.get_roles():
+        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+
+@frappe.whitelist()
+def add_approver(owner):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_approver_id")
+    if purchase_approver != '' or purchase_approver != None:
+        return purchase_approver
