ec4355044271ae5e10b7937cabe8c2d1bba10d0e
Doreen2002 <doreenmwapekatebe8@gmail.com>
2024-09-17 13:49:30 +0000

    report file configure

---
+++ erp/erpnext/report/purchase_order_custom/purchase_order_custom.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Mukesh and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Purchase Order Custom"] = {
+	"filters": [
+
+	]
+};
